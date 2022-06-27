f0b3b010b7190d123d40ba5f06243f0be855451b
Ishaan Walecha <64886059+ishaanw007@users.noreply.github.com>
2022-06-27 12:18:59 +0530

    hello

--- config/database.js
+++ config/database.js
@@ -2,9 +2,9 @@ module.exports = ({ env }) => ({
   connection: {
     client: "mysql",
     connection: {
-      host: env("DATABASE_HOST", "127.0.0.1"),
+      host: env("DATABASE_HOST", "localhost"),
       port: env.int("DATABASE_PORT", 3306),
-      database: env("DATABASE_NAME", "map"),
+      database: env("DATABASE_NAME", "india"),
       user: env("DATABASE_USERNAME", "root"),
       password: env("DATABASE_PASSWORD", "ishaan"),
       ssl: env.bool("DATABASE_SSL", false),
